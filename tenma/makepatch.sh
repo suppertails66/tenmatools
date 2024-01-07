5a8b2e1d773c830287358ad3859001edd7f5f890
Supper <suppertails66@gmail.com>
2024-01-06 21:15:17 -0600

    Update makepatch.sh for v1.2

--- tenma/makepatch.sh
+++ tenma/makepatch.sh
@@ -1,4 +1,4 @@
-versionnum="v1.1"
+versionnum="v1.2"
 
 # filename_iso="patch/auto_patch/Tengai Makyou - Ziria EN [${versionnum}-iso].xdelta"
 # filenameredump="patch/redump_patch/Tengai Makyou Ziria EN [${versionnum}] Redump.xdelta"
